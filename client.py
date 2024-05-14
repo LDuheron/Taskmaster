d5ec9297dc1ceb991eb81731a7f46c9252ed41c2
Lisa <lisa.duheron@dauphine.eu>
2024-05-14 15:43:48 +0200

    clean + functional terminal

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-
 # module pour creer des cli plus facilement, definit les commandes que lutilisateur peut saisir et les reponses corespondantes
 # # gerer les erreurs de commande avec methode default appelee quand lutililisateur entre une commande invalide
 import cmd
@@ -6,8 +5,7 @@ import cmd
 #pour lire char par char
 import sys, tty, termios
 
-
-# This function allows to read the stdin in raw mode (meaning, without expecting the '\n')
+# This function allows reading stdin in raw mode (meaning, without expecting the '\n' to transmit the data)
 # only one character per character
 def read_char():
 	fd = sys.stdin.fileno()
@@ -20,24 +18,19 @@ def read_char():
 		termios.tcsetattr(fd, termios.TCSADRAIN, old_tty_setting)
 
 
-# # classe cmd 
+# start stop restart status
 
 class InputInterpretor(cmd.Cmd):
-	prompt = 'Taskmaster >'
-	history = []
+	prompt = 'Taskmaster > '
 
 	def do_start(self, arg):
-	#checker si l'arg existe
-	print("Restart command :) ")
+		#checker si l'arg corespond au config file
+		print("Restart command :) ")
 
-	def cmd_loop()
-		while (42)
+	def cmd_loop(self):
+		while (42):
 			char = read_char()
-			if (char = '\x1b[A')
-				#manage history
-			else
-				inputInterpretor.cmd_loop()
-
+			self.cmdloop(char)
 
 if __name__ == "__main__":
-	InputInterpretor().cmd_loop()
+	InputInterpretor().cmdloop()
