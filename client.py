829c95aa192bc74d96bc79e0b600890d6b5730b9
tsiguenz <43640980+tsiguenz@users.noreply.github.com>
2024-06-07 18:11:29 +0200

    Non blocking ipc (#9)
    * Initializes client side + add doc + start parse command
    
    * protected functions from empty command
    
    * finished check of arguments
    
    * transmitting value from client
    
    * add send data in client, improved handle connection
    
    * fixed the missing connect in clien
    
    * Fixed the broken pipe problem + handle client disconnection
    
    * clean
    
    * change error handling in init server from a panic to a match
    
    * did a cargo fmt
    
    * non blocking tcp stream seems working
    
    * wip mutli clients
    
    * fix client to use multy clients connections
    
    * remove useless stuff
    
    * refactoring tcp connection
    
    * refactoring and server respond to requests send by the client
    
    * add emptyline and comment in client after review
    
    ---------
    
    Co-authored-by: Lisa <lisa.duheron@dauphine.eu>

--- client.py
+++ client.py
@@ -1,37 +1,72 @@
 import cmd
 import sys, tty, termios
 import socket
+import signal
+import sys
+import os
+
+
+HOST = 'localhost'
+PORT = 4241
 
 # This function allows reading stdin in raw mode (meaning, without expecting the '\n' to transmit the data)
 # only one character per character
 def read_char():
 	fd = sys.stdin.fileno()
 	old_tty_setting = termios.tcgetattr(fd)
-	new_tty_setting = termios.tcgetattr(fd)
 	try:
 		tty.setraw(fd, termios.TCSADRAIN)
 		return sys.stdin.read(1)
 	finally:
 		termios.tcsetattr(fd, termios.TCSADRAIN, old_tty_setting)
 
+def	send_data(str):
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+		sock.send(str.encode('utf-8'))
+		print(sock.recv(128))
+		sock.close()
+	except Exception as e:
+		print("Error when sending data:", e)
+
 class InputInterpretor(cmd.Cmd):
 	prompt = 'Taskmaster > '
 
+	def	__init__(self):
+		super().__init__()
+
+	def	default(self, line):
+		print(f"{line} is invalid...")
+		print("Correct format is: [command] [program]")
+		print("Type 'help' for all commands.")
+	
+	def do_status(self, arg):
+		"""Print the status of all the programs"""
+		send_data(f"status {arg}")
+
 	def do_start(self, arg):
-		'start the arg'
-		# checker si l'arg corespond au config file
-		print("Start command :) ")
+		"""Start the program specified in argument"""
+		send_data(f"start {arg}")
 	
 	def do_stop(self, arg):
-		print("Stop command")
+		"""Stop the program specified in argument"""
+		send_data(f"stop {arg}")
 
-	def do_restart(self, arg):
-		print("Restart command")
+	def do_restart(self, arg=None):
+		"""Restart the program specified in argument"""
+		send_data(f"restart {arg}")
 	
 	def do_quit(self, arg):
-		return True
-	
-	def	emptyline(self):
+		"""Disconnect the client and quit program"""
+		sys.exit(0)
+
+	def do_reload(self, arg):
+		"""Reload the config file"""
+		os.system("pkill -hup taskmaster")
+		print("taskmaster config file is reloaded")
+
+	def emptyline(self):
 		pass
 
 	def cmd_loop(self):
@@ -39,5 +74,9 @@ class InputInterpretor(cmd.Cmd):
 			char = read_char()
 			self.cmdloop(char)
 
+def signal_handler(sig, frame):
+    sys.exit(0)
+
 if __name__ == "__main__":
+	signal.signal(signal.SIGINT, signal_handler)
 	InputInterpretor().cmdloop()
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod parser;
 
 use error::{Error, Result};
 use parser::config::Config;
+use std::io::prelude::*;
+use std::net::TcpListener;
 
 const SIGHUP: i32 = 1;
 static mut RELOAD_CONFIG: bool = false;
@@ -17,10 +19,59 @@ extern "C" fn handle_sighup(_signum: i32) {
     }
 }
 
-fn main() -> Result<()> {
+fn try_reload_config(config: &mut Config, config_file: &String) {
     unsafe {
-        signal(SIGHUP, handle_sighup as usize);
+        if RELOAD_CONFIG {
+            match config.reload_config(&config_file) {
+                Err(e) => println!("log: can't reload file: {e}"),
+                Ok(()) => println!("log: config file is reloaded: \n{:#?}", config),
+            }
+            RELOAD_CONFIG = false;
+        }
     }
+}
+
+fn server_routine(listener: &TcpListener, config: &mut Config, config_file: &String) -> Result<()> {
+    let duration = std::time::Duration::from_millis(100);
+    for stream in listener.incoming() {
+        try_reload_config(config, config_file);
+        match stream {
+            Ok(mut s) => {
+                let mut data: [u8; 128] = [0; 128];
+                let bytes_read: usize = s
+                    .read(&mut data)
+                    .map_err(|e| Error::Default(e.to_string()))?;
+                if bytes_read == 0 {
+                    std::thread::sleep(duration);
+                    continue;
+                }
+                let formatted = String::from_utf8_lossy(&data[..bytes_read]);
+                println!("read: {}", formatted);
+                // do something with the message from the client
+                // and return a message
+                // is it a fatal error ?
+                s.write(b"Success").map_err(|e| Error::IO(e.to_string()))?;
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                std::thread::sleep(duration)
+            }
+            Err(e) => return Err(Error::IO(e.to_string())),
+        }
+    }
+    Ok(())
+}
+
+fn init_connection(ip: String, port: String) -> Result<TcpListener> {
+    let listener =
+        TcpListener::bind(format!("{ip}:{port}")).map_err(|err| Error::Default(err.to_string()))?;
+    listener
+        .set_nonblocking(true)
+        .map_err(|err| Error::Default(err.to_string()))?;
+    println!("bind ok");
+    Ok(listener)
+}
+
+fn main() -> Result<()> {
     if std::env::args().len() != 2 {
         return Err(Error::BadNumberOfArguments(String::from(
             "usage: taskmaster config_file",
@@ -30,16 +81,10 @@ fn main() -> Result<()> {
     let mut config: Config = Config::new();
     config.parse_config_file(&config_file)?;
     println!("{:#?}", config);
-    let duration = std::time::Duration::from_millis(500);
-    loop {
-        unsafe {
-            if RELOAD_CONFIG {
-                config.reload_config(&config_file)?;
-                println!("{:#?}", config);
-                RELOAD_CONFIG = false;
-            }
-        }
-        std::thread::sleep(duration);
+    unsafe {
+        signal(SIGHUP, handle_sighup as usize);
     }
-    // Ok(())
+    let listener: TcpListener = init_connection("localhost".into(), "4241".into())?;
+    server_routine(&listener, &mut config, &config_file)?;
+    Ok(())
 }
